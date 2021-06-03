8856501cf9146ca8996625db34ad0c99fd49e287
Brandon Roff <45691205+Roffo66@users.noreply.github.com>
2021-06-03 10:52:55 +0100

    Add files via upload

---
+++ PiPush-v2.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+PASSWORD=upvgtnar666@
+num=235
+host=192.168.195.${num}
+	[ ! -d /NAS/IT/RPI/Pi-Sever/PiBlast ] && mkdir -p /NAS/IT/RPI/Pi-Sever/PiBlast/
+	localpath=/NAS/IT/RPI/Pi-Sever/PiBlast/
+	remotepath="${host}:/usr/local/bin"
+	#echo $remotepath
+	sudo rsync -avz --rsh="sshpass -p ${PASSWORD} ssh -l root -oStrictHostKeyChecking=accept-new" "$localpath" "$remotepath"
+
+PASSWORD=upvgtnar666@
+num=61
+host=192.168.195.${num}
+	[ ! -d /NAS/IT/RPI/Pi-Sever/PiBlast ] && mkdir -p /NAS/IT/RPI/Pi-Sever/PiBlast/
+	localpath=/NAS/IT/RPI/Pi-Sever/PiBlast/
+	remotepath="${host}:/usr/local/bin"
+	#echo $remotepath
+	sudo rsync -avz --rsh="sshpass -p ${PASSWORD} ssh -l root -oStrictHostKeyChecking=accept-new" "$localpath" "$remotepath"
+
+PASSWORD=upvgtnar666@
+num=7
+host=192.168.195.${num}
+	[ ! -d /NAS/IT/RPI/Pi-Sever/PiBlast ] && mkdir -p /NAS/IT/RPI/Pi-Sever/PiBlast/
+	localpath=/NAS/IT/RPI/Pi-Sever/PiBlast/
+	remotepath="${host}:/usr/local/bin"
+	#echo $remotepath
+	sudo rsync -avz --rsh="sshpass -p ${PASSWORD} ssh -l root -oStrictHostKeyChecking=accept-new" "$localpath" "$remotepath"
